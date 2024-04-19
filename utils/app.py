2dbc9630148000d821a7337852be0dc60a6eff74
Sebastian Bush <sekbush22@gmail.com>
2024-04-19 19:04:52 -0400

    Update app.py

--- utils/app.py
+++ utils/app.py
@@ -45,6 +45,8 @@ def test():
     #headers['referer'] = st.secrets.mls.referer
     #headers['origin'] = st.secrets.mls.origin
 
+    time.sleep(10)
+
     sched_next = requests.get(st.secrets.mls.next_url, 
                               headers={'referer': st.secrets.mls.referer, 'origin':st.secrets.mls.origin,
                               'user-agent': 'Mozilla/5.0 (Windows; U; MSIE 9.0; Windows NT 9.0; en-US);'})
