7d66455520218561d4d058ad3e1fbad894a9004c
Sebastian Bush <157786091+skbush22@users.noreply.github.com>
2024-04-19 14:25:18 -0400

    Add files via upload

---
+++ Homepage.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+st.set_page_config(
+        page_title="Homepage",
+)
+
+st.title('MLS/NWSL Stats App')
+st.subheader('By: Sebastian Bush @mlstat, @nwslstat on X')
+st.divider()
+
+st.text('''This is an ongoing project that I will be doing my best to update  
+and grow as time goes on. Currently, the supported graphics are only   
+player reports for MLS players in the 2024 season. There may be some formatting   
+quirks so bear with me. I appreciate your patience!''')
+
+
+st.text('''If you have any suggestions for features or things to add in  
+the future, please let me know! You can reach me on X!''')
+
