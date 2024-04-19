639bffc4d4579d07a5543022cadf7d6f2a91e34a
Sebastian Bush <sekbush22@gmail.com>
2024-04-19 19:48:56 -0400

    Update test_app.py

--- utils/test_app.py
+++ utils/test_app.py
@@ -5,8 +5,8 @@ import requests
 def test():
 
   sched_next = requests.get(st.secrets.mls.next_url, 
-    headers={'referer': st.secrets.mls.referer, 'origin':st.secrets.mls.origin,
-    'user-agent': 'Mozilla/5.0 (Windows; U; MSIE 9.0; Windows NT 9.0; en-US);'}, proxies={'http':'http://38.154.227.167'})
+    headers={'referer': "https://optaplayerstats.statsperform.com/en_GB/soccer", 'origin':"https://optaplayerstats.statsperform.com/en_GB/soccer",
+    'user-agent': 'Mozilla/5.0 (Windows; U; MSIE 9.0; Windows NT 9.0; en-US);'})
   sched = sched_next.text
 
   return sched
