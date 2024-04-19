5a14008a6ed1bb33f7c82434fde47fdd2532de76
Sebastian Bush <sekbush22@gmail.com>
2024-04-19 19:39:17 -0400

    Update test_app.py

--- utils/test_app.py
+++ utils/test_app.py
@@ -5,8 +5,8 @@ import requests
 def test():
 
     sched_next = requests.get(st.secrets.mls.next_url, 
-                              headers={'referer': st.secrets.mls.referer, 'origin':st.secrets.mls.origin,
-                              'user-agent': 'Mozilla/5.0 (Windows; U; MSIE 9.0; Windows NT 9.0; en-US);'})
+      headers={'referer': st.secrets.mls.referer, 'origin':st.secrets.mls.origin,
+      'user-agent': 'Mozilla/5.0 (Windows; U; MSIE 9.0; Windows NT 9.0; en-US);'})
     sched = sched_next.text
 
     return sched
