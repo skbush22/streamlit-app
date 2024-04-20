052b69efd4af215ca4e9d105c36b0243c3843f69
Sebastian Bush <sekbush22@gmail.com>
2024-04-20 00:04:16 -0400

    Update test_app.py

--- utils/test_app.py
+++ utils/test_app.py
@@ -5,7 +5,8 @@ import requests
 def test():
 
   sched_next = requests.get("https://api.performfeeds.com/soccerdata/match/qxcx5jmswgto1qeqcjzghtddt/?_rt=c&tmcl=929cd2ue4bpcebvx91rx9e3h0&live=yes&_pgSz=400&_pgNm=2&_lcl=en&_fmt=jsonp&_clbk=W32b7874e6f7b21140cce4e44b90e48dcf2e8bf48b", 
-    headers={'referer': "https://optaplayerstats.statsperform.com/en_GB/soccer"
+    headers={'referer': "https://optaplayerstats.statsperform.com/en_GB/soccer",
+    'User-Agent':''
     })
   sched = sched_next.headers
   text = sched_next.text[:100]
